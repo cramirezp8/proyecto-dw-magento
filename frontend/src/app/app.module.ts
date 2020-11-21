1e7158773c7846cbbb3a91708aec352cf87d7ad8
cramirezp8 <60749643+cramirezp8@users.noreply.github.com>
2020-11-21 16:50:59 -0600

    actualizacion
    segundo avance

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { PlanesComponent } from './components/planes/planes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    LoginComponent,
+    RegistroComponent,
+    PlanesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static('public')); //use se utiliza para ejecutar middlewares
-
-app.listen('4200', function() {
-    console.log('Servidor levantado')
-});
